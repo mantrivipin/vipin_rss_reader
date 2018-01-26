cc2ac56adbc0d912f87d3355ca5c244a411e7f66
mantrivipin17 <mantrivipin17@gmail.com>
2018-01-26 22:51:39 +0000

    Added link to entries in index page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,5 +2,6 @@ class EntriesController < ApplicationController
 
   def index
     @entries = Entry.where(feed_id: current_user.feeds.pluck(:id))
+                    .order('published_at desc')
   end
 end
